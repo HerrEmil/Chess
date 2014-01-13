35daa7fa17da96348e8c4c092f1caf84ce1d7e1e
Emil Andersson <ean@anthillagency.com>
2014-01-13 17:37:24 +0100

    Castling tests

--- js/test.js
+++ js/test.js
@@ -267,47 +267,28 @@ function arrayToReadable(arrayPosition) {
 function castleCheckTest() {
     'use strict';
     $('td').html('');
+    $('#18').html('<a href="#" class="white rook">&#9820;</a>');
+    $('#22').html('<a href="#" class="white rook">&#9820;</a>');
     $('#56').html('<a href="#" class="white rook">&#9820;</a>');
-    // $('#57').html('<a href="#" class="white knight">&#9822;</a>');
-    // $('#58').html('<a href="#" class="white bishop">&#9821;</a>');
-    // $('#11').html('<a href="#" class="white queen">&#9819;</a>');
-    $('#60').html('<a href="#" class="white king">&#9818;</a>');
-    // $('#61').html('<a href="#" class="white bishop">&#9821;</a>');
-    // $('#62').html('<a href="#" class="white knight">&#9822;</a>');
     $('#63').html('<a href="#" class="white rook">&#9820;</a>');
-    // $('#48').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#49').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#50').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#51').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#52').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#53').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#54').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#55').html('<a href="#" class="white pawn">&#9823;</a>');
-    // $('#0').html('<a href="#" class="black rook">&#9820;</a>');
-    $('#41').html('<a href="#" class="black knight">&#9822;</a>');
-    // $('#2').html('<a href="#" class="black bishop">&#9821;</a>');
-    // $('#3').html('<a href="#" class="black queen">&#9819;</a>');
+
+    $('#60').html('<a href="#" class="white king">&#9818;</a>');
+
+    $('#0').html('<a href="#" class="black rook">&#9820;</a>');
+    $('#7').html('<a href="#" class="black rook">&#9820;</a>');
+    $('#43').html('<a href="#" class="black rook">&#9820;</a>');
+    $('#45').html('<a href="#" class="black rook">&#9820;</a>');
+
     $('#4').html('<a href="#" class="black king">&#9818;</a>');
-    // $('#5').html('<a href="#" class="black bishop">&#9821;</a>');
-    // $('#45').html('<a href="#" class="black knight">&#9822;</a>');
-    // $('#7').html('<a href="#" class="black rook">&#9820;</a>');
-    // $('#8').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#9').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#10').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#11').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#12').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#13').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#14').html('<a href="#" class="black pawn">&#9823;</a>');
-    // $('#15').html('<a href="#" class="black pawn">&#9823;</a>');
     game.board = [
         '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
         '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
-        '*', '-', '-', '-', '-', 'K', '-', '-', '-', '*',
+        '*', 'R', '-', '-', '-', 'K', '-', '-', 'R', '*',
+        '*', '-', '-', '-', '-', '-', '-', '-', '-', '*',
+        '*', '-', '-', 'r', '-', '-', '-', 'r', '-', '*',
         '*', '-', '-', '-', '-', '-', '-', '-', '-', '*',
         '*', '-', '-', '-', '-', '-', '-', '-', '-', '*',
-        '*', '-', '-', '-', '-', '-', '-', '-', '-', '*',
-        '*', '-', '-', '-', '-', '-', '-', '-', '-', '*',
-        '*', '-', 'N', '-', '-', '-', '-', '-', '-', '*',
+        '*', '-', '-', '-', 'R', '-', 'R', '-', '-', '*',
         '*', '-', '-', '-', '-', '-', '-', '-', '-', '*',
         '*', 'r', '-', '-', '-', 'k', '-', '-', 'r', '*',
         '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
